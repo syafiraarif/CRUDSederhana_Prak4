6060b09c8b8996c3836774f5680103bdddc2aa2a
syairaarif <putrisyafira4444@gmail.com>
2025-03-21 14:24:39 +0700

    perintah MessageBox.Show akan menampilkan pesan dalam bentuk box

--- Form1.cs
+++ Form1.cs
@@ -134,7 +134,7 @@ namespace CRUDSederhana
                                 }
                                 else
                                 {
-
+                                    MessageBox.Show
                                 }
                             }
 
