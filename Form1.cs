43fec7ade352be1b24c510f34f024e116a1b3d27
syairaarif <putrisyafira4444@gmail.com>
2025-03-21 13:36:29 +0700

    perintah MessageBoxIcon.Question berfungsi untuk menampilkan icon quetion berupa tanda tanya

--- Form1.cs
+++ Form1.cs
@@ -109,7 +109,8 @@ namespace CRUDSederhana
         {
             if (dgvMahasiswa.SelectedRows.Count > 0)
             {
-                DialogResult confirm = MessageBox.Show("Yakin ingin menghapus data ini?", "Konfirmasi", MessageBoxButtons.YesNo,
+                DialogResult confirm = MessageBox.Show("Yakin ingin menghapus data ini?", "Konfirmasi", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
             }
         }
 
