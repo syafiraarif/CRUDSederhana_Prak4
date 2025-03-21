b2e38649160c3aa05849f925b55d49e1b0a311f5
syairaarif <putrisyafira4444@gmail.com>
2025-03-21 13:37:20 +0700

    membuat if atau perulangan dalam perulangan, karena dalam perulangan tedapat 2 kondisi yang kemungkinan terjadi

--- Form1.cs
+++ Form1.cs
@@ -110,6 +110,10 @@ namespace CRUDSederhana
             if (dgvMahasiswa.SelectedRows.Count > 0)
             {
                 DialogResult confirm = MessageBox.Show("Yakin ingin menghapus data ini?", "Konfirmasi", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if ()
+                {
+
+                }
 
             }
         }
