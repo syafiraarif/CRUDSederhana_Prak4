cb2c3c4392d8fb9b0bd941830203227599c4c04f
syairaarif <putrisyafira4444@gmail.com>
2025-03-21 12:44:23 +0700

    messagebox.show ini digunakan untuk menampikan pesan kepada pengguna

--- Form1.cs
+++ Form1.cs
@@ -91,7 +91,7 @@ namespace CRUDSederhana
                         }
                         else
                         {
-
+                            MessageBox.Show
                         }
 
                     }
