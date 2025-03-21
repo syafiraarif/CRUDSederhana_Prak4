a1d96fe3fbaed4474b65acfed90c6b0e0807eb9c
syairaarif <putrisyafira4444@gmail.com>
2025-03-21 12:34:47 +0700

    ini adalah peulangan yang dijslankan dalam prosedur tambah

--- Form1.cs
+++ Form1.cs
@@ -83,6 +83,10 @@ namespace CRUDSederhana
                         cmd.Parameters.AddWithValue("@Alamat", txtAlamat.Text.Trim());
 
                         int rowsAffected = cmd.ExecuteNonQuery();
+                        if ()
+                        {
+
+                        }
 
                     }
 
