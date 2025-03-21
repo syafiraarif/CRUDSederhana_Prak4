ce8af8e69ecd4954ffbf2f6b7abd15798ef86c18
syairaarif <putrisyafira4444@gmail.com>
2025-03-21 13:55:22 +0700

    WHERE NIM = @NIM";  ini maksudnya adalah hanya menghaus data dengan nim tertentu yang sudah dipilih dan sesuai dengan nilai parameternya

--- Form1.cs
+++ Form1.cs
@@ -118,7 +118,8 @@ namespace CRUDSederhana
                         {
                             string nim = dgvMahasiswa.SelectedRows[0].Cells["NIM"].Value.ToString();
                             conn.Open();
-                            string query = "DELETE FROM Mahasiswa 
+                            string query = "DELETE FROM Mahasiswa WHERE NIM = @NIM";
+
 
                         }
                     }
