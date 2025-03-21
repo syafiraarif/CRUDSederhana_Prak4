1290007f2b4404a9b9155d4a6a2707af60f9e7c9
syairaarif <putrisyafira4444@gmail.com>
2025-03-21 13:48:49 +0700

    perintah Cells["NIM"]. nantinya akan mengambil kolom NIM yang sudah pengguna pilh

--- Form1.cs
+++ Form1.cs
@@ -116,7 +116,7 @@ namespace CRUDSederhana
                     {
                         try
                         {
-                            string nim = dgvMahasiswa.SelectedRows[0].
+                            string nim = dgvMahasiswa.SelectedRows[0].Cells["NIM"].
                         }
                     }
  
