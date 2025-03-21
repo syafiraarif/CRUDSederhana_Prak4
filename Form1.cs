a45b0e8e17e10c7a663f747f88bf7830d7411fcb
syairaarif <putrisyafira4444@gmail.com>
2025-03-21 13:49:59 +0700

    perintah Value.ToString(); akan mengubah nilai data tersebut menjadi string dan menyimpannya dalam variabel nim

--- Form1.cs
+++ Form1.cs
@@ -116,7 +116,8 @@ namespace CRUDSederhana
                     {
                         try
                         {
-                            string nim = dgvMahasiswa.SelectedRows[0].Cells["NIM"].
+                            string nim = dgvMahasiswa.SelectedRows[0].Cells["NIM"].Value.ToString();
+
                         }
                     }
  
