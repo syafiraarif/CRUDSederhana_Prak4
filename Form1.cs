2ba25e37b42ace9827b18e09e60673a8ae7fad6e
syairaarif <putrisyafira4444@gmail.com>
2025-03-21 15:47:16 +0700

    perintah Cells["NIM"] digunakan untuk mengakses sel dalam baris berdasarkan kolom "NIM"

--- Form1.cs
+++ Form1.cs
@@ -171,7 +171,7 @@ namespace CRUDSederhana
             {
                 DataGridViewRow row  = dgvMahasiswa.Rows[e.RowIndex];
                  
-                txtNIM.Text = row
+                txtNIM.Text = row.Cells["NIM"]
 
             }
         }
