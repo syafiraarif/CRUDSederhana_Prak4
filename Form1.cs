6a7802119a8fc95df68aae1d365456c1691da992
syairaarif <putrisyafira4444@gmail.com>
2025-03-21 12:00:48 +0700

    return figunakan untuk menghentikan metode yang sedang berjalan sekarang dan keluar dari metode tersebut

--- Form1.cs
+++ Form1.cs
@@ -71,6 +71,7 @@ namespace CRUDSederhana
                     if (txtNIM.Text == "" || txtNama.Text == "" || txtEmail.Text == "" || txtTelepon.Text == "" || txtAlamat.Text == "")
                     {
                         MessageBox.Show("Harap isi semua data!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
 
                     }
 
