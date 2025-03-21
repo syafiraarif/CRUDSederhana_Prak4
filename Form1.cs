ba51702325af04558925fa87ac4c7efc20b290e7
syairaarif <putrisyafira4444@gmail.com>
2025-03-21 11:44:15 +0700

    perintah ini nantinya akan mengisi objek DataTable dengan data dari database dengan menggunakan SqlDataAdapter yang telah kita buat

--- Form1.cs
+++ Form1.cs
@@ -44,6 +44,9 @@ namespace CRUDSederhana
                     conn.Open();
                     string query = "SELECT NIM AS [NIM], Nama, Email, Telepon, Alamat FROM Mahasiswa";
                     SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
 
 
                 }
