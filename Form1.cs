674276a890733e44553b4a78fcebf59db8dbc691
syairaarif <putrisyafira4444@gmail.com>
2025-03-21 13:41:23 +0700

    using digunakan untuk memastikan bahwa koneksi ke databasse akan otomatis tertutup tanpa menggunakan conn.close

--- Form1.cs
+++ Form1.cs
@@ -112,7 +112,7 @@ namespace CRUDSederhana
                 DialogResult confirm = MessageBox.Show("Yakin ingin menghapus data ini?", "Konfirmasi", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
                 if (confirm == DialogResult.Yes)
                 {
-
+                    using()
                 }
 
             }
