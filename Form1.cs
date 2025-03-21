ce88de239ecff9bdafce1a5c2fc51e54c1d364cf
syairaarif <putrisyafira4444@gmail.com>
2025-03-21 15:17:38 +0700

    "Debugging DataGridView" merupakan judul dari messagebox yang ditampilkan

--- Form1.cs
+++ Form1.cs
@@ -161,7 +161,8 @@ namespace CRUDSederhana
         {
             LoadData();
 
-            MessageBox.Show($"Jumlah Kolom: {dgvMahasiswa.ColumnCount}\nJumlah Baris:{dgvMahasiswa.RowCount}", 
+            MessageBox.Show($"Jumlah Kolom: {dgvMahasiswa.ColumnCount}\nJumlah Baris:{dgvMahasiswa.RowCount}",
+                 "Debugging DataGridView",
 
         }
 
